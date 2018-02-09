f3335cdbd6e3f170f23dd65c5ec9a67a52a8339f
Toli Carter <drachis@drachis.com>
2018-02-08 19:27:26 -0800

    base dragterma project for collaboration

---
+++ Dragterma/Assets/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float baseSpeed = 1;
+    public float speedRange, startingPosition, startingPositionVariation, maximumPosition, heightRange;
+    public GameObject projectile;
+    // Use this for initialization
+    private float speed;
+    void Start()
+    {
+        Respawn();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        // fly forward
+        gameObject.transform.localPosition = new Vector3(
+            gameObject.transform.localPosition.x - speed * Time.deltaTime,
+            gameObject.transform.localPosition.y,
+            gameObject.transform.localPosition.z
+            );
+        if (gameObject.transform.localPosition.x < maximumPosition)
+        {
+            Respawn();
+        }
+        Debug.Log(projectile.GetComponent<Collider>().tag);
+        if (projectile.GetComponent<Collider>().tag == "Player") {
+            Respawn();
+        }
+        
+    }
+
+    void Respawn()
+    {
+        // start with a random position inside the screen and with some speed variation
+        speed = Random.Range(baseSpeed - speedRange, baseSpeed + speedRange);
+        gameObject.transform.localPosition = new Vector3(
+            startingPosition + Random.value * startingPositionVariation,
+            Random.Range(-heightRange, heightRange),
+            gameObject.transform.localPosition.z);
+    }
+}
---
+++ Dragterma/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float dodgeRange = 1;
+    public float speed = 1;
+    public GameObject ship;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private Vector3 controledParentPosition, controlledChildPosition;    
+	void Update () {
+        //update container object position
+        controledParentPosition = new Vector3(
+            gameObject.transform.localPosition.x,
+            gameObject.transform.localPosition.y + 
+                (speed * Time.deltaTime * Input.GetAxis("Vertical")),
+            gameObject.transform.localPosition.z);
+        gameObject.transform.localPosition = controledParentPosition;
+        // update child object position
+        controlledChildPosition = new Vector3(
+            ship.transform.localPosition.x,
+            Input.GetAxis("Vertical") * dodgeRange,
+            ship.transform.localPosition.z);
+        ship.gameObject.transform.localPosition = controlledChildPosition;
+    }
+    
+}
